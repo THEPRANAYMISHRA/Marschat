36aa2491a3dd9f2ac9d9c7495af7b030703af671
Pranay <pmcanvas4501@gmail.com>
2023-11-12 10:56:17 +0530

    origin changed

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: 'https://marschat.netlify.app/',
+        origin: '*',
         methods: ["GET", "POST"]
     }
 });
